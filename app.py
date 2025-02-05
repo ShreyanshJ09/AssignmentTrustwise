7367aa23e0416fa16530692294f04baacd4fa424
Shreyansh Jangitwar <f20201446@pilani.ac.in>
2025-02-05 21:10:29 +0530

    some changes

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def history():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port,debug=True)
     # app.run(debug=True)
